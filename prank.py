01002ac87d20f544c345f9bc82fc70d12e333535
Cody Bernardy <codybernardyphotos@gmail.com>
2017-10-28 16:56:20 -0700

    Adding prank.py and prank.txt

---
+++ prank.py
@@ -0,0 +1,14 @@
+import webbrowser
+import time
+
+"""
+This file loops opening tabs of a certain youtube video
+DO NOT USE THIS WITHOUT THE OWNER'S PERMISSION
+"""
+
+def browser():
+	while True:
+		webbrowser.open_new_tab("https://youtu.be/YqeW9_5kURI?t=44s")
+		time.sleep(3)
+		break
+browser()
