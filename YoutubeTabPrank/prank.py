bcfc5895bfe7be15861503bbb3305598878daf5f
Cody Bernardy <codybernardyphotos@gmail.com>
2017-10-28 23:01:20 -0700

    Final Touches

--- YoutubeTabPrank/prank.py
+++ YoutubeTabPrank/prank.py
@@ -1,5 +1,5 @@
 import webbrowser
-import time
+#import time
 
 """
 This file loops opening tabs of a certain youtube video
@@ -7,8 +7,7 @@ DO NOT USE THIS WITHOUT THE OWNER'S PERMISSION
 """
 
 def browser():
-	while True:
-		webbrowser.open_new_tab("https://youtu.be/YqeW9_5kURI?t=44s")
-		time.sleep(3)
-		break
+	"""while True: """	"""This will open up a shit ton of browsers and tabs with this song, make sure to tab in
+		below """
+	webbrowser.open_new_tab("https://youtu.be/YqeW9_5kURI?t=44s")
 browser()
