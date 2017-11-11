8315b1ce6ee7cb42c1da85b39d5e84ec869e4952
Cody <root@localhost.localdomain>
2017-11-10 17:08:10 -0800

    Not sure what Im pushing, but im pushing it!

--- YoutubeTabPrank/prank.py
+++ YoutubeTabPrank/prank.py
@@ -7,8 +7,7 @@ DO NOT USE THIS WITHOUT THE OWNER'S PERMISSION
 """
 
 def browser():
-	while True:
-		webbrowser.open_new_tab("https://youtu.be/YqeW9_5kURI?t=44s")
-		time.sleep(3)
-		break
+
+	webbrowser.open_new_tab("https://youtu.be/YqeW9_5kURI?t=44s")
+
 browser()
