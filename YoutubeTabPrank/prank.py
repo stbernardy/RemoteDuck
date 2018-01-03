f015b9d83bbbcd54228ddd00d84b8a5f41d23d26
Cody Bernardy <codybernardyphotos@gmail.com>
2018-01-02 19:12:04 -0800

    Removing garbage text

--- YoutubeTabPrank/prank.py
+++ YoutubeTabPrank/prank.py
@@ -7,12 +7,8 @@ DO NOT USE THIS WITHOUT THE OWNER'S PERMISSION
 """
 
 def browser():
-<<<<<<< HEAD
-
 	webbrowser.open_new_tab("https://youtu.be/YqeW9_5kURI?t=44s")
-
-=======
 	#Add a while loop in this for more carnage
 	webbrowser.open_new_tab("https://youtu.be/YqeW9_5kURI?t=44s")
->>>>>>> 9b4f3fabb5e773182a3c6444480509eebcb1a770
+	
 browser()
